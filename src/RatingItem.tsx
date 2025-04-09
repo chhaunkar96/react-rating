8a49d180391266120427ae2668c3095cfae86629
Kaushal  Chhaunkar <chhaunkar96@gmail.com>
2025-04-09 23:50:23 +0530

    size updated

--- src/RatingItem.tsx
+++ src/RatingItem.tsx
@@ -137,7 +137,7 @@ export function RatingItem({
          aria-hidden="true"
          className={RatingClasses.SVG}
          xmlns="http://www.w3.org/2000/svg"
-         viewBox={'-0.5 -0.5 28 28'}
+         viewBox={'-0.5 -0.5 20 20'}
          preserveAspectRatio="xMidYMid meet"
          {...getStrokeAttribute()}
          {...testId}
